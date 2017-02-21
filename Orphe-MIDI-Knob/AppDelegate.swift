8b639b1b6cbbb60c63e07eb0f0bbba8cd8f5b432
nokkii <nokkii.03@gmail.com>
2017-02-21 18:36:39 +0900

    Initial Commit

---
+++ Orphe-MIDI-Knob/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Orphe-MIDI-Knob
+//
+//  Created by noaki-takuya on 2017/02/21.
+//  Copyright © 2017年 noaki-takuya. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Orphe-MIDI-Knob/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Orphe-MIDI-Knob
+//
+//  Created by noaki-takuya on 2017/02/21.
+//  Copyright © 2017年 noaki-takuya. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
